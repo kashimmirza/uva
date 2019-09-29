9c1a6506271b791b7fff6273ba49d7f3359c0f44
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-09-29 20:04:11 +0600

    learningslogan.cpp

---
+++ learningslogan.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string a,b;
+    int k1,k2;
+    cin>>k1;
+    getchar();
+    for(int i=0;i<k1;i++){
+       // cin>>a>>b;
+
+
+
+
+       getline(cin,a);
+       getline(cin,b);
+    }
+
+
+    int m;
+
+    cin>>m;
+   getchar();
+    string l;
+    for(int i=0;i<m;i++){
+            getline(cin,l);
+            if(l=="jalo re jalo"){
+
+                cout<<"agun jalo"<<endl;
+                //getchar();
+            }
+            else {
+
+                cout<<"tui rajakar tui rajakar"<<endl;
+
+
+            }
+
+
+    }
+
+
+}
