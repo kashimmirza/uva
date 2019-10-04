b20920d5e3d5cbd71ae7038b0f077df92f83a654
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-10-04 10:52:31 +0600

    validtriangle.cpp

---
+++ validtraingle.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int a,b,c;
+  int sum;
+  int n;
+  cin>>n;
+  while(n--){
+ cin>>a>>b>>c;
+ // sum=0;
+ sum=a+b+c;
+ if(sum==180){
+    cout<<"YES"<<endl;
+
+
+ }
+ else
+    cout<<"NO"<<endl;
+ }
+}
+
+
