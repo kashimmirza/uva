6c6895853423c4ff646384e680147e1a21c7c1e1
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-10-04 12:26:30 +0600

    carrynotcarry.cpp

---
+++ carrynottocarry.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<cstdio>
+using namespace std;
+int main(){
+int a,b,c,result=0;
+
+while(scanf("%d%d",&a,&b)!=EOF){
+
+ int re=a^b;
+
+ cout<<re<<endl;
+
+
+
+}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
