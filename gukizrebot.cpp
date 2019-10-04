ab844aa8918f70aa9b91b390c396de5df24c6eb4
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-10-04 10:50:05 +0600

    gukizrebot.cpp

---
+++ gukizrebot.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int x,y,x1,y2;
+ int r1=0,r2=0,mi=0;
+ cin>>x>>y>>x1>>y2;
+ r1=abs(x-x1);
+ r2=abs(y-y2);
+ if(r1>r2){
+
+    cout<<r1<<endl;
+ }
+ else
+    cout<<r2<<endl;
+
+
+
+
+
+}
