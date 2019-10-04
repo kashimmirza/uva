4cb0544bc7bbc933093b3a6c3480e95fcfa800e9
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-10-04 10:48:54 +0600

    enormousinput.cpp

---
+++ enormousinput.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+ int n,k,input,sum=0;
+ cin>>n>>k;
+ while(n--){
+   cin>>input;
+   if(input%k==0){
+
+      sum++;
+
+   }
+
+
+
+ }
+ cout<<sum<<endl;
+
+
+
+
+
+
+
+}
